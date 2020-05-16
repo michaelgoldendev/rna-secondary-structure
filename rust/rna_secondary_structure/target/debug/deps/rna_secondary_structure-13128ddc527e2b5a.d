4c2ae62cc8d19c2cf35a46ea4ffbbf6f5077b4c0
michaelgoldendev <michaelgolden0@gmail.com>
2020-05-16 22:25:31 +0100

    {MG} Moved .gitignore

--- rust/rna_secondary_structure/target/debug/deps/rna_secondary_structure-13128ddc527e2b5a.d
+++
@@ -1,7 +0,0 @@
-/home/michael/GitHub/rna-secondary-structure/rust/rna_secondary_structure/target/debug/deps/rna_secondary_structure-13128ddc527e2b5a: src/main.rs src/secondary_structure.rs src/./secondary_structure_test.rs
-
-/home/michael/GitHub/rna-secondary-structure/rust/rna_secondary_structure/target/debug/deps/rna_secondary_structure-13128ddc527e2b5a.d: src/main.rs src/secondary_structure.rs src/./secondary_structure_test.rs
-
-src/main.rs:
-src/secondary_structure.rs:
-src/./secondary_structure_test.rs:
